ef3a18ad643e87455267886e3d4b8f2d96ee5414
Hugo <hugo.suzuki@ontariotechu.net>
2019-12-02 18:14:44 -0500

    [MOD] Drag/Drop fully functional
    Toaster and Garbage also working

--- Fast Food Simulator/Assets/Assets/Scripts/Ingredient.cs
+++ Fast Food Simulator/Assets/Assets/Scripts/Ingredient.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Ingredient : MonoBehaviour
 {
@@ -12,11 +13,13 @@ public class Ingredient : MonoBehaviour
     public bool isBread;
     public bool toasted;
 
+    private bool toToast;
+    private bool garbageToDelete;
+
     // Start is called before the first frame update
     void Start()
     {
         initPos = transform.position;
-        
     }
 
     // Calculate the difference between click location and object position
@@ -31,7 +34,55 @@ public class Ingredient : MonoBehaviour
     {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector2(mousePos.x - dX, mousePos.y - dY);
+    }
 
+    private void OnMouseUp()
+    {
+        if (toToast)
+        {
+            toasted = true;
+        }
+        if (garbageToDelete)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collided");
+        if (collision.gameObject.name == "toaster")
+        {
+            Debug.Log("Toaster Coll");
+            if (isBread)
+            {
+                toToast = true;
+            }
+        }
+
+        if (collision.gameObject.name == "garbage")
+        {
+            Debug.Log("Garbage Coll");
+            garbageToDelete = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("Coll Out");
+        if (collision.gameObject.name == "toaster")
+        {
+            Debug.Log("Toaster Out");
+            if (isBread)
+            {
+                toToast = false;
+            }
+        }
+
+        if (collision.gameObject.name == "garbage")
+        {
+            Debug.Log("Garbage Out");
+            garbageToDelete = false;
+        }
     }
 
     // Update is called once per frame
