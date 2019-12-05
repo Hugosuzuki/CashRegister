94df3ca4816841fa49f071890b6c94cb38bd0745
Hugo <hugo.suzuki@ontariotechu.net>
2019-12-04 19:31:56 -0500

    [MOD] Finish Button

---
+++ Fast Food Simulator/Assets/Assets/Scripts/AddScoreTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.EventSystems;
+
+public class AddScoreTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        Score.ChangeScore(50);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
